ca4acf9903e47d2ad1d36da2863019a6e97bb2f2
desshub <desshub95@gmail.com>
2018-04-17 13:37:04 +0300

    finished on method to generate thumbnail

--- routes/protected.js
+++ routes/protected.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const verifytoken = require('../middlewares/verifytoken');
 const config = require('../config/config');
 const jsonpatch = require('jsonpatch'); 
+const jimp = require('jimp');
 
 app.use(verifytoken); 
 
@@ -15,19 +16,55 @@ app.get('/', (req, res) => {
     res.status(200).json( req.decoded );
   });
 
+  // POST route to pass a json object and patch json arrays then return a json patched document
 app.post('/json_patch', (req, res) => {
     let theObj = req.body.obj;
     let thePatch = req.body.patch;
 
-    console.log(thePatch);
-    if(theObj && thePatch) {
-      let patcheddoc = jsonpatch.apply_patch(theObj, thePatch);
-      res.status(200).json(patcheddoc);
+    if(theObj && thePatch) {  // checks if json object and json patch array have been passed from the from the body
+      let patcheddoc = jsonpatch.apply_patch(theObj, thePatch); // Applies a patch to theObj
+      res.status(200).json({
+        success:true,
+        document: patcheddoc
+      });
     }else{
       res.status(400).json({
         success: false,
         message: "Please provide obj (JSON) and patch (JSON Array) on the form"
       })
+    }
+});
+
+// Post endpoint to generate image thumbnail from a url
+app.post('/generate_thumnail', (req,res) => {
+    let imgurl = req.body.imgurl;
+
+    if(imgurl) {
+      jimp.read(imgurl, function (err, thumbnail) {
+        if (err){
+          console.log(err);
+          res.status(400).json({
+            success: false,
+            message: err
+          })
+        }else{
+        thumbnail.resize(100, jimp.AUTO)            // resize
+             .quality(60)                 // set JPEG quality
+             .getBase64(jimp.AUTO, (error, thumb) => {
+               if (error) throw err;
+               res.status(200).json({
+                 success: true,
+                 message: "Thumbnail generated successfully",
+                 thumbnail: thumb    // return the image thumnail
+               });
+             }); 
+            };
+      });
+    }else{
+      res.status(400).json({
+        success: false,
+        message: "Please provide/pass image url (imgurl) on the form"
+      });
     }
 })
 
